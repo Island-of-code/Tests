90c7ec9f4d43cee04abb5d8b6c7e8578e69e3ba1
volyasmith <vladikuznetsov@gmail.com>
2016-11-11 16:46:06 +0500

    11

--- app.js
+++ app.js
@@ -1,67 +1,178 @@
-'use strict'
-var myApp = angular.module('myApp', ['ngEventAggregator']);
+"use strict";
+var myApp = angular.module("myApp", ["ngEventAggregator"]);
 
-myApp.controller('MainController', ['$scope', 'eventAggregator', function ($scope, eventAggregator) {
-    
-    
-    $scope.onKeyUp = function(event) {
+myApp.controller("MainController",
+[
+    "$scope", "eventAggregator", function($scope, eventAggregator) {
 
-        if (event.key === "ArrowRight") {
-            eventAggregator.trigger('arrowRight.happens', { event: event });
+        window.onkeydown = function(event) {
+            if (event.key === "ArrowRight") {
+                eventAggregator.trigger("rightDown.happens", { event: event });
+            }
+            if (event.key === "ArrowLeft") {
+                eventAggregator.trigger("leftDown.happens", { event: event });
+            }
+
+            if (event.key === "ArrowUp") {
+                eventAggregator.trigger("upDown.happens", { event: event });
+            }
+
+
+        };
+        window.onkeyup = function(event) {
+            if (event.key === "ArrowRight") {
+                eventAggregator.trigger("rightUp.happens", { event: event });
+            }
+            if (event.key === "ArrowLeft") {
+                eventAggregator.trigger("leftUp.happens", { event: event });
+            }
+
+            if (event.key === "ArrowUp") {
+                eventAggregator.trigger("upUp.happens", { event: event });
+            }
+        };
+
+        function Game(canvasElement, eventAggregator) {
+
+            var controlEvent = {
+                ship: {}
+            };
+
+            var ctx = canvasElement.getContext("2d");
+            var ship = null;
+            var gameLoopInterval = null;
+            var glyphsTree = {
+                ship: null,
+                shots : []
+        };
+
+            eventAggregator.on("rightDown.happens",
+                function() {
+                    controlEvent.ship.dx = 1;
+                });
+
+            eventAggregator.on("rightUp.happens",
+                function() {
+                    controlEvent.ship.dx = 0;
+                });
+
+            eventAggregator.on("leftDown.happens",
+                function() {
+                    controlEvent.ship.dx = -1;
+                });
+
+            eventAggregator.on("leftUp.happens",
+                function() {
+                    controlEvent.ship.dx = 0;
+                });
+
+            eventAggregator.on("upUp.happens",
+                function() {
+                    controlEvent.shot = false;
+                });
+
+            eventAggregator.on("upDown.happens",
+                function() {
+                    controlEvent.shot = true;
+                });
+
+            this.run = function() {
+                ship = new Ship(ctx);
+                glyphsTree.ship = ship;
+                gameLoopInterval = setInterval(loop, 33);
+            };
+
+            function renderCollectionAndFilter(glyphs, controlEvent) {
+
+                var forDelete = [];
+
+                for (var i = 0; i < glyphs.length; i++) {
+
+                    if (glyphs.destroy) {
+                        forDelete.push(i);
+                    } else {
+                        glyphs[i].renderObject(controlEvent);
+                    }
+                }
+
+                for (var i = 0; i < forDelete.length; i++) {
+                    glyphs.splice(forDelete[i], 1);
+                }
+            }
+
+
+            function loop() {
+
+                renderCanvas();
+
+                glyphsTree.ship.renderObject(controlEvent);
+
+                if (controlEvent.shot) {
+                    glyphsTree.shots.push(new Shot(ctx, ship.x));
+                }
+
+                renderCollectionAndFilter(glyphsTree.shots, controlEvent);
+            }
+
+            function renderCanvas() {
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(0, 0, 600, 600);
+            }
+
         }
-        if (event.key === "ArrowLeft") {
-            eventAggregator.trigger('arrowLeft.happens', { event: event });
+
+
+        function Shot(ctx, x) {
+
+            var that = this;
+            this.destroy = false;
+
+            var canvasH = ctx.canvas.height;
+
+            this.x = x;
+            var y = 10;
+
+            var height = 10;
+            var width = 5;
+
+            this.renderObject = function(message) {
+                if (that.destroy) {
+                    return;
+                }
+                y++;
+                if (y > canvasH)
+                    that.destroy = true;
+
+                ctx.fillStyle = "#FF0000";
+                ctx.fillRect(that.x, y, height, width);
+            };
         }
-        //console.log(event);
+
+
+        function Ship(ctx) {
+
+            var that = this;
+            this.x = 10;
+            this.height = 10;
+            this.width = 10;
+            this.destroy = false;
+
+            this.renderObject = function(controlEvent) {
+
+                if (controlEvent.ship.dx) {
+                    that.x += controlEvent.ship.dx;
+                }
+
+                ctx.fillStyle = "#FF0000";
+                ctx.fillRect(that.x, 10, that.height, that.width);
+            };
+        }
+
+        var canvasElement = document.getElementById("canvas");
+        $scope.greeting = "Hola!";
+
+        var game = new Game(canvasElement, eventAggregator);
+        game.run();
+
     }
-
-    function MainScreen(drawCtx, eventAggregator) {
-        
-        var drawCtx = drawCtx;
-        var laser = null;
-        var eventAggregator = eventAggregator;
-
-        this.addLaser = function() {
-
-            laser = new Laser(drawCtx);
-            return laser;
-        }
-
-        eventAggregator.on('arrowRight.happens', onArrowRight);
-        eventAggregator.on('arrowLeft.happens', onArrowLeft);
-
-        function onArrowRight(data) {
-            laser.draw();
-        }
-
-        function onArrowLeft(data) {
-            laser.draw();
-        }
-
-    }
-
-    function Laser(drawCtx) {
-        var that = this;
-        var drawCtx = drawCtx;
-        this.x = 10;
-        this.y = 10;
-
-        this.draw = function () {
-            drawCtx.beginPath();
-            drawCtx.arc(95, 50, 40, 0, 2 * Math.PI);
-            drawCtx.stroke();
-        }
-
-    }
-
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.moveTo(0, 0);
-    context.lineTo(200, 100);
-    context.stroke();
-    $scope.greeting = 'Hola!';
-
-    var mainScreen = new MainScreen(context, eventAggregator);
-    mainScreen.addLaser();
-
-}]);
+]);
